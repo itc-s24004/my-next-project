057e72f0cb3b06c28e484da8292f42fb1cc9fbc2
itc-s24004 <s24004@std.it-college.ac.jp>
2025-01-14 16:28:45 +0900

    メンバーページにmicroCMSを組み込み

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,14 +1,48 @@
-export type Category = {
-    name: string;
-}
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSListContent,
+} from "microcms-js-sdk";
+export type Member = {
+  name: string;
+  position: string;
+  profile: string;
+  image: MicroCMSImage;
+} & MicroCMSListContent;
 
+export type Category = {
+  name: string;
+};
 
 export type News = {
-    id: string;
-    title: string;
-    category: {
-        name: string;
-    };
-    publishedAt: string;
-    createdAt: string;
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MMICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Member>({
+    endpoint: "members",
+    queries,
+  });
+  return listData;
 };
