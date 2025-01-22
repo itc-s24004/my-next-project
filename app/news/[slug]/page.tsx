db3e3af023d066c34ef6a7d521314da74ea69906
s24004 <s24004@std.it-collgege.ac.jp>
2025-01-22 16:13:14 +0900

    ch7完成

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -7,17 +7,22 @@ import { notFound } from "next/navigation";
 type Props = {
     params: {
         slug: string;
-    }
-}
+    };
+    searchParams: {
+        dk?: string;
+    };
+};
 
-export default async function Page({params}: Props) {
-    const data = await getNewsDetail(params.slug).catch(notFound);
+export default async function Page({ params, searchParams }: Props) {
+    const data = await getNewsDetail(params.slug, {
+        draftKey: searchParams.dk,
+    }).catch(notFound);
     return (
         <>
-            <Article data={data}/>
+            <Article data={data} />
             <div className={styles.footer}>
                 <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
             </div>
         </>
-    )
+    );
 }
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,9 +1,20 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
+import { NEWS_LIST_LIMIT } from "../_components";
+import Pagination from "../_components/Pagination";
+import SearchField from "../_components/SearchField";
 
 
 
 export default async function Page() {
-    const { contents: news } = await getNewsList();
-    return <NewsList news={news} />;
+    const { contents: news, totalCount } = await getNewsList({
+        limit: NEWS_LIST_LIMIT
+    });
+    return (
+        <>
+            <SearchField />
+            <NewsList news={news} />
+            <Pagination totalCount={totalCount} />
+        </>
+    );
 }
